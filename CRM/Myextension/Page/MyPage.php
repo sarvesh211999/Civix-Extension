bdc9528af10ccbbb2ddda3080f294c6251d8d46f
sarvesh211999 <sarvesh211999@gmail.com>
2019-03-16 23:36:59 +0000

    access

--- CRM/Myextension/Page/MyPage.php
+++ CRM/Myextension/Page/MyPage.php
@@ -7,17 +7,19 @@ class CRM_Myextension_Page_MyPage extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('My Page'));
     $session = CRM_Core_Session::singleton();
-	$contact_id = $session->get('userID');
-	$params = array(
-    	'id' => $contact_id,
-  	);
- 
-   	$result = civicrm_api3('Contact', 'getsingle', $params);
-	// echo $result['first_name'];
+    $contact_id = $session->get('userID');
+    if ($contact_id){
+        $params = array(
+        'id' => $contact_id,
+        );
+        $result = civicrm_api3('Contact', 'getsingle', $params);
+        $this->assign('firstName', $result['first_name']);
+    }
+    else{
+        $this->assign('firstName', "World");
+    }
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-    $this->assign('firstName', $result['first_name']);
+    // // Example: Assign a variable for use in a template
 
     parent::run();
   }
